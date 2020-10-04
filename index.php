2f9b27b90adcbc16e9ef19f89e7d6055559638e9
Johnny Liang <zhonglin.liang@sjsu.edu>
2020-10-03 21:31:14 -0700

    test

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
         $list = explode(" ",$contact_string);
         $_GET["Email"] = $list[0];
         $_GET["Tel"] = $list[1];
-        echo $_GET["Email"];
-        echo $_GET["Tel"];
+        echo $_GET;
     }
 ?>
