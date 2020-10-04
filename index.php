6da06adb8cbdc38beb4ba2a881c593a521a067f1
Johnny Liang <zhonglin.liang@sjsu.edu>
2020-10-03 20:08:11 -0700

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 // http_response_code(200);
 // $subject = $_GET('fname');
-$contact = file(./resources/contact.txt);
+$contact = file("./resources/contact.txt");
 $list = explode(" ",$contact);
 echo $list;
 
