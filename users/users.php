58de02d79828335788cda7f14e66fb388d4e3e3a
Johnny Liang <zhonglin.liang@sjsu.edu>
2020-10-10 00:53:47 -0700

    testing

--- users/users.php
+++ users/users.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: http://localhost:3000");
 
     if($_GET){
         echo json_encode("<div>Hello World</div>");
