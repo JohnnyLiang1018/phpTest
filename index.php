fd1e7a40f3e18cef00a601b4fa91f431fa3ba456
Johnny Liang <zhonglin.liang@sjsu.edu>
2020-10-13 01:33:53 -0700

    admin login

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
 <?php
     header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers");
     
     if($_GET){
         $contact = file("./resources/contact.txt");
         $contact_string = implode($contact);
         $list = explode(" ",$contact_string);
         echo json_encode($list);
+    }
+
+    if($_POST){
+        $json = file_get_contents('php://input');
+        $data = json_decode($json);
+        if(!$data->username || !$data->password){
+            echo json_encode("Login failed");
+        }
+        else{
+            if($data->username == "admin" && $data->password == "password"){
+                $users = file("./resources/users.txt");
+                $users_string = implode($users);
+                $list = explode(",",$users_string);
+                echo json_encode($list);
+            }
+            else{
+                echo json_encode("Login failed");
+            }
+        }
     }
 ?>
--- users/users.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header("Access-Control-Allow-Origin: http://localhost:3000");
-
-    if($_GET){
-        echo json_encode("<div>Hello World</div>");
-    }
-
-?>
