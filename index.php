f6f272f39855c053f6b5e8377f9c6257b903af91
Johnny Liang <zhonglin.liang@sjsu.edu>
2020-10-17 01:53:48 -0700

    fix header

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Origin: https://sevensea.herokuapp.com");
     header("Access-Control-Allow-Headers");
     
     if($_GET){
