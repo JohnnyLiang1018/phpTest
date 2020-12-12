2b9c75ca968b3d0bf226a101a1323d7e5e4cdd02
Johnny Liang <zhonglin.liang@sjsu.edu>
2020-12-12 09:35:40 -0800

    addReview

--- review.php
+++ review.php
@@ -11,11 +11,7 @@
         $data = json_decode($json);
         $url = sprintf("https://ancient-retreat-00756.herokuapp.com/php_files/Hw_files/addReviewCloud.php?product=%s&rating=%s&overwrite=True&text_review=%s&email=&company_affiliation=sevensea",$data->product,$data->rating,$data->review);
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_SSL_VERIFYPEER , 0);
         curl_setopt($ch,CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         $fp = curl_exec($ch);
         curl_close($ch);
         echo json_encode(array('code'=>200, 'message'=>'Review Added'));
